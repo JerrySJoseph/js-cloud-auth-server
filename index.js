50c567d36d64d3c4d63dc1134332d23d1374c69c
JerrySJoseph <jerin.sebastian153@gmail.com>
2021-04-21 14:04:16 +0530

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/**
+ * This is a revision for dynamic database 
+ */
+
+
 const authEngine = require("./js-auth");
 const cloudApp = require("./js-core");
 const messagingEngine = require("./js-cloud-messaging");
